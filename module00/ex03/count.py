4f478cef0ea920c5986b081ad2ccfb66cefd9f09
Charles MARIOT <contact@charles-mariot.fr>
2021-11-08 14:44:51 +0100

    ex00 -> ex05

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,5 +1,35 @@
-def text_analyser():
-    print("Hello world");
+import sys
 
-if __name__ == "__main__":
-    text_analyser();
+def text_analyser(*args):
+    """This function counts the number of upper characters, lower characters, punctuation and spaces in a given text."""
+    if (len(args) == 0):
+        str = input("What is the text to analyse ?\n")
+    elif (len(args) > 1):
+        print ("ERROR");
+        return ;
+    else:
+        str = args[0];
+    i = 0;
+    characters = 0;
+    upper_char = 0;
+    lower_char = 0;
+    punctuation = 0;
+    spaces = 0;
+    while (i < len(str)):
+        if (str[i] == ' '):
+            spaces = spaces + 1;
+        elif (str[i] >= 'a' and str[i] <= 'z'):
+            lower_char = lower_char + 1;
+        elif (str[i] >= 'A' and str[i] <= 'Z'):
+            upper_char = upper_char + 1;
+        elif (str[i] >= '!' and str[i] <= '/'):
+            punctuation = punctuation + 1;
+        elif (str[i] >= ':' and str[i] <= '?'):
+            punctuation = punctuation + 1;
+        characters = characters + 1;
+        i = i + 1;
+    print("The text contains", characters, "characters :");
+    print("-", upper_char, "upper letters");
+    print("-", lower_char, "lower letters");
+    print("-", punctuation, "punctuation marks");
+    print("-", spaces, "spaces");
---
+++ module00/ex04/operations.py
@@ -0,0 +1,62 @@
+import sys
+
+def get_argc():
+    return (len(sys.argv) - 1);
+
+def atoi(str):
+    number = 0
+    sign = 1;
+    if (str[0] == '-'):
+        sign = -1;
+        str = str[1:];
+    elif (str[0] == '+'):
+        str = str[1:];
+    if (len(str) == 0):
+        print ("InputError: only numbers")
+        print("Usage: python operations.py <number1> <number2>");
+        print("Example:");
+        print("\tpython operations.py 10 3");
+        return None;
+    i = 0
+    while (i < len(str)):
+        if (str[i] >= '0' and str[i] <= '9'):
+            number = number * 10 + (ord(str[i]) - ord('0'))
+        else:
+            print ("InputError: only numbers")
+            print("Usage: python operations.py <number1> <number2>");
+            print("Example:");
+            print("\tpython operations.py 10 3");
+            return None;
+        i = i + 1
+    return (sign * number);
+
+def elementary(str1, str2):
+    number1 = atoi(str1);
+    if (number1 == None):
+        return ;
+    number2 = atoi(str2);
+    if (number2 == None):
+        return ;
+    print("Sum :\t\t", number1 + number2);
+    print("Difference :\t", number1 - number2);
+    print("Product :\t", number1 * number2);
+    if (number2 != 0):
+        print("Quotient :\t", number1 / number2);
+        print("Remainder :\t", number1 % number2);
+    else:
+        print("Quotient :\t ERROR (div by zero)");
+        print("Remainder :\t ERROR (modulo by zero)");
+
+if __name__ == "__main__":
+    
+    if (len(sys.argv) == 0):
+        print("Usage: python operations.py <number1> <number2>");
+        print("Example:");
+        print("\tpython operations.py 10 3");
+    elif (len(sys.argv) > 3):
+        print("InputError: too many arguments");
+        print("Usage: python operations.py <number1> <number2>");
+        print("Example:");
+        print("\tpython operations.py 10 3");
+    else:
+        elementary(sys.argv[1], sys.argv[2]);
