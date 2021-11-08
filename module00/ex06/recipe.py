ae9eaaf6ce99c0205c318e6498cf9246ae646c46
Charles MARIOT <contact@charles-mariot.fr>
2021-11-08 16:39:05 +0100

    Backup

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -1,1 +1,6 @@
 if __name__ == "__main__":
+    cookbook = {sandwich = {'ingredients': 'ham, bread and tomatoes', 'meal': 'lunch', 'prep_time': '10 minutes'},
+                cake = {'ingredients': 'flour, suggar and eggs', 'meal': 'dessert', 'prep_time': '60 minutes'},
+                salad = {'ingredients': 'avocado, arugula, tomatoes and spinach', 'meal': 'lunch', 'prep_time': '15 minutes'}}
+    for keys, value in cookbook.items():
+        print(cookbook.keys());
