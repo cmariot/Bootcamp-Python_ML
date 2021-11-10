0487fd1744ea330a5018343f24588d96392f5f44
Charles Mariot <contact@charles-mariot.fr>
2021-11-10 15:40:24 +0100

    The vectors are initialized

--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -6,7 +6,7 @@
 #    By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/09 23:56:15 by cmariot           #+#    #+#              #
-#    Updated: 2021/11/10 13:48:15 by cmariot          ###   ########.fr        #
+#    Updated: 2021/11/10 15:36:17 by cmariot          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,12 @@ class Vector:
         type_list_of_list = False;
         type_list_of_list_of_floats = False;
         type_list_of_floats = False;
+        type_int = False;
+        type_range = False;
+        column_vector = False
+        row_vector = False
 
-        # Check the input value type for the init, it could be a list of list of floats or a list of floats 
+        # Check the input value type for the init, it could be a list of list of floats, a list of floats, an int or a range.
         if (type(value) == list):
             type_list = True;
             if (type(value[0]) == list):
@@ -38,6 +42,10 @@ class Vector:
             else:
                 print("The vector must be initialized as a list of list of floats or a list of floats.")
                 return ;
+        elif (type(value) == int):
+            type_int = True;
+        elif (type(value) == range):
+            type_range = True;
         else:
             print("The vector must be initialized as a list of list of floats or a list of floats.")
             return ;
@@ -47,24 +55,52 @@ class Vector:
        # print("type_list_of_list_of_floats = " + str(type_list_of_list_of_floats));
        # print("type_list_of_floats = " + str(type_list_of_floats));
 
+        self.value = []
+        
         if (type_list_of_list_of_floats == True or type_list_of_floats == True):
-            x1 = value[0]
-            x2 = value[1]
-            x3 = value[2]
-            x4 = value[3]
-            self.value = [x1, x2, x3, x4];
+            for elements in value:
+                self.value.append(elements)
+            if (type_list_of_list_of_floats == True):
+                column_vector = True;
+                self.shape = (len(self.value), 1)
+            elif (type_list_of_floats == True):
+                row_vector = True;
+                self.shape = (1, len(self.value))
 
-        column_vector = False
-        row_vector = False
-        if (type_list_of_list_of_floats == True):
-            column_vector = True;
-        elif (type_list_of_floats == True):
-            row_vector = True;
+        elif (type_int == True):
+            i = 0.0
+            while (i < float(value)):
+                self.value.append([i])
+                i += 1.0
+            self.shape = (int(i), 1);
+
+        elif (type_range == True):
+            min = float(value[0])
+            print(min)
+            max = float(value[-1])
+            print(max)
+            range_size = max - min
+            while min <= max:
+                self.value.append([min])
+                min += 1.0
+            self.shape = (int(range_size) + 1, 1)
 
 
 if __name__ == "__main__":
+    
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
     print(v1.value)
+    print(v1.shape)
+    
     v2 = Vector([0.0, 1.0, 2.0, 3.0])
     print(v2.value)
+    print(v2.shape)
+
+    v3 = Vector(3)
+    print(v3.value)
+    print(v3.shape)
+
+    v4 = Vector(range(10, 15))
+    print(v4.value)
+    print(v4.shape)
 
