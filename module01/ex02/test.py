1f62c79d280e38a59eff4a4e59d9e3ce40964347
Charles Mariot <contact@charles-mariot.fr>
2021-11-11 17:36:43 +0100

    Vector

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -6,108 +6,121 @@
 #    By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/09 23:56:10 by cmariot           #+#    #+#              #
-#    Updated: 2021/11/11 02:28:38 by cmariot          ###   ########.fr        #
+#    Updated: 2021/11/11 17:31:40 by cmariot          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from vector import Vector
 
+# Operation on Vector objects, using class, method 
 # Run with "python test.py"
 if __name__ == "__main__":
 
     # INIT
     # Different types to init a vector
-    v0 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    v1 = Vector([[10.0], [2.0], [3.0], [4.0]])
-    v2 = Vector([0.0, 1.0, 2.0, 3.0, 4.0])
-    v3 = Vector([10.0, 2.0, 3.0, 4.0])
-    v4 = Vector(3)
-    v5 = Vector(range(4, 9))
-    v6 = Vector(range(2, 7))
+    v0 = Vector([0.0, 1.0, 2.0, 3.0])
+    v1 = Vector([0.0, 1.0, 2.0, 3.0])
+    v2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v3 = Vector([[1.0], [2.0], [3.0], [4.0]])
+    v4 = Vector(4) #[[0.0], [1.0], [2.0]] (3, 1)
+    v5 = Vector(range(0, 4)) #[[4.0], [5.0], [6.0], [7.0], [8.0]] (5, 1)
+    v6 = Vector(range(1, 5)) #[[2.0], [3.0], [4.0], [5.0], [6.0]] (5, 1)
 
     print("PRINT THE VECTORS AFTER THE INIT")
-    print(v0)
-    print(v1)
-    print(v2)
-    print(v3)
-    print(v6)
-    print(v4)
-    print(v5)
+    print("v0 = ", v0)
+    print("v1 = ", v1)
+    print("v2 = ", v2)
+    print("v3 = ", v3)
+    print("v4 = ", v4)
+    print("v5 = ", v5)
+    print("v6 = ", v6)
 
-    # A Vector have 2 attributes
-    # Attribute value = x1, x2, x3 ... xn
-    # Attribute shape = if type v0 or v1 (number of value , 1) for vector columns
-    #                   else (1, number of value) for vector rows
+    # A Vector have 2 attributes :
+    # Attribute value = [[x1], [x2], [x3] ... [xn]]
+    # and 
+    # Attribute shape = if it's a vector column (number of value , 1) or
+    #                   (1, number of value) for vector row
 
-    # [[0.0], [1.0], [2.0], [3.0]] (4, 1)
-    # [[1.0], [2.0], [3.0], [4.0]] (4, 1)
-    # [0.0, 1.0, 2.0, 3.0] (1, 4)
-    # [10.0, 2.0, 3.0, 4.0] (1, 4)
-    # [[0.0], [1.0], [2.0]] (3, 1)
-    # [[10.0], [11.0], [12.0], [13.0], [14.0]] (5, 1)
-    # [[15.0], [16.0], [17.0], [18.0], [19.0]] (5, 1)
-    # [[0.0], [1.0], [2.0], [3.0]]
-
+    # v0 =  [0.0, 1.0, 2.0, 3.0] (1, 4)
+    # v1 =  [0.0, 1.0, 2.0, 3.0] (1, 4)
+    # v2 =  [[0.0], [1.0], [2.0], [3.0]] (4, 1)
+    # v3 =  [[1.0], [2.0], [3.0], [4.0]] (4, 1)
+    # v4 =  [[0.0], [1.0], [2.0], [3.0]] (4, 1)
+    # v5 =  [[0.0], [1.0], [2.0], [3.0]] (4, 1)
+    # v6 =  [[1.0], [2.0], [3.0], [4.0]] (4, 1)[0.0], [1.0], [2.0], [3.0]] (4, 1)
 
     print("\nPRINT ATTRIBUTES")
     print(v0.value)
     print(v0.shape)
+    print()
 
     # METHODS
-    # Like function on Vector 
+    # Like function on Vecto, definitions in vector.py
     # Addition, substraction, division, multiplication, Transpose or dot product between two vectors of same shape
+    print("OPERATIONS")
     print("Addition")
-    print(v0.__str__())
+    print(v0.__str__(), " + ", v1.__str__(),  " = ", end="") 
     v0.__add__(v1);
     print(v0.__str__())
     v0.__str__()
-    print()
 
 
     v2.__add__(v3);
 
     # Return addition
-    #v0.__radd__(v1);
-    #v2.__radd__(v3);
+    v0.__radd__(v1);
+    v2.__radd__(v3);
 
     # Substraction
+    print("Substraction")
     v1.__sub__(v0);
     v3.__sub__(v2);
+    print("v3 - v2 = " + v3.__str__())
 
-    # Return addition
-    #v0.__rsub__(v1);
-    #v2.__rsub__(v3);
-
-    # Division
-    v0.__truediv__(5);
-    v2.__truediv__(5);
-
-    # Return addition
-    #v0.__rtruediv__(5);
-    #v2.__rtruediv__(5);
+    # Return substraction
+    v0.__rsub__(v1);
+    v2.__rsub__(v3);
 
     # Multiplication
-    v0.__mul__(5);
-    v2.__mul__(5);
+    print("Multiplication")
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v1.__mul__(5)
+    print("v1 * 5 = " + v1.__str__())
 
-    # Return addition
+    # Return multiplication
     v0.__rmul__(5);
     v2.__rmul__(5);
 
+    # Division
+    print("Division");
+    v0.__truediv__(5);
+    v2.__truediv__(5);
+    print("v2 / 5 =" + v2.__str__())
+
+    # Return division
+    v2.__rtruediv__(3.14);
+    #v2.__rtruediv__(5);
+
     # Transpose
+    print("Transposition")
+    
+    print("v0 = ", v0)
     v0.T()
+    print ("v0.t()")
+    print("v0 = ", v0)
     v2.T()
 
     # Dot product between two vectors of same shape
-    #result = v1.dot(v2)
-    result = v0.dot(v3)
+    # x.y = x1.x1 + ... + xn.yn
+    print("Dot product")
+    print(v6.__str__() + " dot " + v3.__str__() + " = ", end="")
+    result = v6.dot(v3)
+    print(result.__str__())
 
     # Print Vector
     v0.__str__()
 
     # Return Vector
     v0.__repr__()
-
-    print("END")
 
 # Go to vector.py to see the Class !
